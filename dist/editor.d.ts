cbe19fb5a1dbdfa8ec432a2a224636dd11de4b4c
Alex Slubsky <aslubsky@gmail.com>
2016-03-01 11:04:45 +0200

    init

---
+++ dist/editor.d.ts
@@ -0,0 +1,6 @@
+import { ElementRef, OnInit } from 'angular2/core';
+export declare class TrumbowygEditor implements OnInit {
+    private el;
+    constructor(el: ElementRef);
+    ngOnInit(): void;
+}
